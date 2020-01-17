de122490a619416316f201d745241a63416c22df
Fdom92 <fer.olmo92@gmail.com>
2020-01-17 10:40:59 +0100

    fix(): add missing load function

--- src/definitions.ts
+++ src/definitions.ts
@@ -5,6 +5,7 @@ declare module "@capacitor/core" {
 }
 
 export interface CapacitorGameKitPlugin {
+  load(): Promise<any>;
   auth(): Promise<any>;
   signOut(): Promise<any>;
   isSignedIn(): Promise<any>;
